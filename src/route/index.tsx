176523c5695f38666e3a1dee17f5c3407f7f74bd
CT <c13240891337@163.com>
2022-09-07 17:20:31 +0800

    init

---
+++ src/route/index.tsx
@@ -0,0 +1,13 @@
+
+import { ReactElement } from "react";
+import { Routes,Route } from "react-router-dom";
+
+const RouterConfig = () : ReactElement => {
+    return (
+        <Routes>
+            <Route path="/"></Route>
+        </Routes>
+    )
+};
+
+export default RouterConfig;
